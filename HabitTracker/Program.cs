2271963252d415ad6e456f24282f9c3b767916d9
ramyabojja92-afk <ramyabojja92@gmail.com>
2026-05-06 11:44:23 +0200

    Add basic menu navigation

---
+++ HabitTracker/Program.cs
@@ -0,0 +1,42 @@
+﻿bool running = true;
+
+while (running)
+{
+    Console.WriteLine("===== Habit Tracker =====");
+    Console.WriteLine("1. Add Habit");
+    Console.WriteLine("2. View Habits");
+    Console.WriteLine("3. Mark Habit as Done");
+    Console.WriteLine("4. Delete Habit");
+    Console.WriteLine("5. Exit");
+    Console.Write("Choose an option: ");
+
+    string? choice = Console.ReadLine();
+
+    if (choice == "1")
+    {
+        Console.WriteLine("Add Habit selected");
+    }
+    else if (choice == "2")
+    {
+        Console.WriteLine("View Habits selected");
+    }
+    else if (choice == "3")
+    {
+        Console.WriteLine("Mark Habit as Done selected");
+    }
+    else if (choice == "4")
+    {
+        Console.WriteLine("Delete Habit selected");
+    }
+    else if (choice == "5")
+    {
+        Console.WriteLine("Goodbye!");
+        running = false;
+    }
+    else
+    {
+        Console.WriteLine("Invalid choice. Please choose 1-5.");
+    }
+
+    Console.WriteLine();
+}
